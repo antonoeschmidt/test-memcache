30cdce1bae9b74afc0d6a4a69dcf17bbb396e7e0
antonoeschmidt <s163053@student.dtu.dk>
2021-11-17 11:26:50 +0100

    changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const cache = require("memory-cache");
+var cache = require("memory-cache");
 const crypto = require("crypto");
-const fs = require("fs")
+const fs = require("fs");
 let testEntry = require("./testEntry.json");
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,32 +13,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/data/:id", (req, res) => {
-    console.log(req.params.id);
     data = cache.get(req.params.id);
+    console.log(cache.keys());
     if (data) {
         res.send(data);
     } else {
-        res.send("No data at endpoint")
+        res.send("No data at endpoint");
     }
 });
 
-app.get("/data/new", (req, res) => {
+app.get("/new", (req, res) => {
 
     // auth with Blockchain here
 
-    fs.readFile('registryOverview.json', function (err, data) {
-        console.log(data);
-        let json = JSON.parse(data)
-        if (json[users][req.body.userid]['health']) {
-            res.send(json[users][req.body.userid]['health'])
-        } else {
-            let randId = crypto.randomBytes(64).toString("hex");
-            cache.put(randId, testEntry, 10000);
-            json[users]['1234']['health'] = randId;
-            res.send(randId);
-        }
-        fs.writeFile('registryOverview.json', JSON.stringify(json))
-    })
+    let randId = crypto.randomBytes(64).toString("hex");
+    cache.put(randId, testEntry, 10000);
+    console.log(cache.keys());
+    res.send(randId);
 });
 
 app.listen(3000);
